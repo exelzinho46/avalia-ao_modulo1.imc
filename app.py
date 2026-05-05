8489e71f07d6118e06e565be2134af7efad5e2e4
hackerman <eliasreserva75@gmail.com>
2026-05-05 09:20:03 -0300

    Update app.py

--- app.py
+++ app.py
@@ -10,23 +10,23 @@ if st.button("Calcular"):
     st.info(f"{imc:.2f}")
     
     if imc < 18.5:
-        st.image("https://multiversonoticias.com.br/wp-content/uploads/2023/08/seu-madruga.PNG")
+       
         st.error("abaixo do peso ")
     
     elif imc < 24.9:
-        st.image("https://midias.correio24horas.com.br/2026/04/16/meme-6-7-viralizou-em-todo-o-mundo-3232308-article.webp")
+        
         st.success("no peso irmao ")
     elif imc < 29.9:
-        st.image("https://midias.correio24horas.com.br/2026/04/16/meme-6-7-viralizou-em-todo-o-mundo-3232308-article.webp")
+        
         st.warning("sobrepeso ")
     elif imc < 34.9:
-        st.image("https://midias.correio24horas.com.br/2026/04/16/meme-6-7-viralizou-em-todo-o-mundo-3232308-article.webp")
+        
         st.error("obesidade ")
     elif imc < 39.9:
-        st.image("https://midias.correio24horas.com.br/2026/04/16/meme-6-7-viralizou-em-todo-o-mundo-3232308-article.webp")
+       
         st.error("obesidade severa ")
     else:
-        st.image("https://midias.correio24horas.com.br/2026/04/16/meme-6-7-viralizou-em-todo-o-mundo-3232308-article.webp")
+       
         st.error("obesidade morbida ")
 
     
